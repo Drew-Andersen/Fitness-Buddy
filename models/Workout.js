374d18d0e918d23375a220d5a4e9acb5bc8de7f8
Drew-Andersen <andrewtandersen@gmail.com>
2024-08-09 14:07:08 -0400

    Added the ./models

---
+++ models/Workout.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Workout extends Model {}
+
+Workout.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        exercise_id: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'exercise',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'workout',
+    }
+)
+
+module.exports = Workout;
