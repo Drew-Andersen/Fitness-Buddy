bdca2a2bab936191ecb51613d581e2c755d10e20
Alex <aadiaz10gmail.com>
2024-08-09 13:54:33 -0500

    updated api/userRoutes.js

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,86 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+const bcrypt = require('bcrypt');
+const withAuth = require('../../utils/auth');
+
+// GET all users info
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single user by id
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST a new user (sign-up)
+router.post('/', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
+
+      res.status(201).json(newUser);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// POST login route
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+
+    if (!user) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+
+      res.json({ user, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// POST logout route
+router.post('/logout', withAuth, (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
