d1a7c7adcdd17c79c7fc3116ae96830870a7d142
Alex <aadiaz10gmail.com>
2024-08-12 12:34:22 -0500

    updating public/js/login.js

---
+++ public/js/login.js
@@ -0,0 +1,27 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if (email && password) {
+      // Send a POST request to the login route
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        // If successful, redirect the browser to the dashboard page
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to log in. Please check your email and password and try again.');
+      }
+    }
+  };
+
+  document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
