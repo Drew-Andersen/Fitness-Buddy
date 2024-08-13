8583bc15ffa819b24e748584e9e64bb4b541098b
Drew-Andersen <andrewtandersen@gmail.com>
2024-08-13 10:49:11 -0400

    Fixed the homeRoute

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const bcrypt = require('bcrypt');
-const withAuth = require('../../utils/auth');
 
 // GET all users info
 router.get('/', async (req, res) => {
@@ -47,7 +45,11 @@ router.post('/', async (req, res) => {
 // POST login route
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ 
+      where: {
+        email: req.body.email 
+      } 
+    });
 
     if (!userData) {
       res.status(400).json({ message: 'Incorrect email or password, please try again' });
@@ -73,7 +75,7 @@ router.post('/login', async (req, res) => {
 });
 
 // POST logout route
-router.post('/logout', withAuth, (req, res) => {
+router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
